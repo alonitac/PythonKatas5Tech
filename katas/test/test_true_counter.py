24f6dc158201c40b1d242289730da5d9e5531d4a
TameerAmer <tameergti@gmail.com>
2025-07-08 11:53:33 +0300

    new changes

--- katas/test/test_true_counter.py
+++ katas/test/test_true_counter.py
@@ -1,18 +1,18 @@
 import unittest
-from katas.true_counter import true_counter
+from katas.true_counter import count_true_values
 
 
 class TestTrueCounter(unittest.TestCase):
     def test_true_count(self):
-        self.assertEqual(true_counter([True, False, True, True, False]), 3)
+        self.assertEqual(count_true_values([True, False, True, True, False]), 3)
 
     def test_empty_list(self):
-        self.assertEqual(true_counter([]), 0)
+        self.assertEqual(count_true_values([]), 0)
 
     def test_no_true_values(self):
-        self.assertEqual(true_counter([False, False, False]), 0)
+        self.assertEqual(count_true_values([False, False, False]), 0)
 
     def test_all_true_values(self):
-        self.assertEqual(true_counter([True, True, True]), 3)
+        self.assertEqual(count_true_values([True, True, True]), 3)
 
 
